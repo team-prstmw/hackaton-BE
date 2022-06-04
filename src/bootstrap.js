2e163695c64bcc9bbae398891a2ca3ca2f04d978
Karol Homziuk <karolhomziuk@gmail.com>
2022-06-04 11:29:52 +0200

    Project init
    project init

---
+++ src/bootstrap.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+import app from './app';
+import env from './constants/env';
+
+let server;
+
+mongoose
+  .connect(env.MONGODB_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.info('Connected to MongoDB');
+
+    server = app.listen(env.PORT, () => {
+      console.info(`Listening to port ${env.PORT}`);
+    });
+  });
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      console.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  console.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  console.info('SIGTERM received');
+
+  if (server) {
+    server.close();
+  }
+});
