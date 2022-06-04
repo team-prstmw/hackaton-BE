27de8e5e390f703c74efcd6b2b3f628f16f5defa
Karol Homziuk <karolhomziuk@gmail.com>
2022-06-04 11:28:50 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+import env from './constants/env';
+import routes from './routes';
+
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors())
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+if(env.NODE_ENV === 'production') {
+    app.use(express.static('public'));
+}
+
+app.use('/api', routes);
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-global-assign
+require = require('esm')(module /* , options */);
+// eslint-disable-next-line import/extensions
+module.exports = require('./bootstrap.js');
